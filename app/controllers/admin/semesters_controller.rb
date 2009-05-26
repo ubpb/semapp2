04870152f542d3f7b79fb4c314d3aab8d2a705d3
René Sprotte <renspr@uni-paderborn.de>
2009-05-26 16:54:07 +0200

    make use of resource_controller plugin

--- app/controllers/admin/semesters_controller.rb
+++ app/controllers/admin/semesters_controller.rb
@@ -1,70 +1,30 @@
 class Admin::SemestersController < Admin::ApplicationController
 
   before_filter :setup_breadcrumb_for_all_actions
-  
-  def index
-    @semesters = Semester.find(:all, :order => 'created_at DESC')
-  end
 
-  def new
-    @semester = Semester.new
-    setup_breadcrumb_for_new_and_create
-  end
+  resource_controller
 
-  def create
-    @semester = Semester.new(params[:semester])
-    setup_breadcrumb_for_new_and_create
-
-    if (@semester.save)
-      flash[:success] = "Semester erfolgreich erstellt"
-      redirect_to :action => 'index'
-    else
-      render :action => 'new'
+  create do
+    wants.html {redirect_to :action => 'index'}
+    flash "Semester erfolgreich erstellt"
+    failure do
+      flash ""
     end
   end
 
-  def edit
-    @semester = Semester.find(params[:id])
-    setup_breadcrumb_for_edit_and_update(@semester)
+  update do
+    wants.html {redirect_to :action => 'index'}
+    flash "Semester erfolgreich aktualisiert"
   end
 
-  def update
-    @semester = Semester.find(params[:id])
-    setup_breadcrumb_for_edit_and_update(@semester)
-
-    @semester.update_attributes(params[:semester])
-      
-    if @semester.save
-      flash[:success] = "Semester erfolgreich aktualisiert"
-      redirect_to :action => 'index'
-    else
-      render :action => 'edit'
-    end
-  end
-
-  def destroy
-    semester = Semester.find(params[:id])
-    if semester and semester.destroy
-      flash[:success] = "Semester '#{semester.title}' erfolgreich gelöscht"
-      redirect_to :action => 'index'
-    else
-      flash[:error] = "Semester konnte nicht gelöscht werden"
-      redirect_to :action => 'index'
-    end
+  destroy do
+    flash "Semester erfolgreich gelöscht"
   end
 
   private
 
   def setup_breadcrumb_for_all_actions
     pui_append_to_breadcrumb("Semester verwalten", admin_semesters_url)
-  end
-
-  def setup_breadcrumb_for_new_and_create
-    pui_append_to_breadcrumb("Neues Semester anlegen", new_admin_semester_url)
-  end
-
-  def setup_breadcrumb_for_edit_and_update(semester)
-    pui_append_to_breadcrumb("Semester bearbeiten", edit_admin_semester_url(semester))
   end
 
 end
