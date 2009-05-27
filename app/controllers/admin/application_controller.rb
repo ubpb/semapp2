b760526bf7182fa9bbc9185fe761b2467c5f00da
René Sprotte <renspr@uni-paderborn.de>
2009-05-27 14:27:43 +0200

    Make shure Admin::* requires admin role

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -1,8 +1,18 @@
 class Admin::ApplicationController < ApplicationController
 
+  before_filter :require_user
+  before_filter :check_for_admin_role
+
   before_filter :setup_admin_breadcrumb
 
   private
+
+  def check_for_admin_role
+    unless current_user.is_admin?
+      flash[:error] = "Zugriff verweigert"
+      redirect_to root_url
+    end
+  end
 
   def setup_admin_breadcrumb
     pui_append_to_breadcrumb("Administration", root_url)
