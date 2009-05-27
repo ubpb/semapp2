eb426d1a7ffedb7812c589cf809287c9e512023b
René Sprotte <renspr@uni-paderborn.de>
2009-05-27 14:41:48 +0200

    make shure the name is valid

--- app/models/authority.rb
+++ app/models/authority.rb
@@ -1,8 +1,9 @@
 class Authority < ActiveRecord::Base
 
-  validates_presence_of :name
-  validates_format_of   :name, :with => /^[A-Z0-9_]+$/
-  validates_length_of   :name, :within => 3..20
+  validates_presence_of   :name
+  validates_uniqueness_of :name
+  validates_format_of     :name, :with => /^[A-Z0-9_]+$/
+  validates_length_of     :name, :within => 3..20
 
   def name=(value)
     write_attribute :name, (value ? value.upcase : nil)
