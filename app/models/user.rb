c6d852e19bbdf046393d12e5388dec406fb29cc5
René Sprotte <renspr@uni-paderborn.de>
2009-05-27 14:13:23 +0200

    create a HBTM realation between users and authorities

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,25 @@
 class User < ActiveRecord::Base
 
+  has_and_belongs_to_many :authorities
+
   acts_as_authentic do |c|
     # for available options see documentation in: Authlogic::ActsAsAuthentic
     c.crypto_provider = Authlogic::CryptoProviders::BCrypt
+  end
+
+  #
+  # Accessor for email field. Always convert to downcase
+  #
+  def email=(value)
+    write_attribute :email, (value ? value.downcase : nil)
+  end
+
+  #
+  # Set the password (and the password_confirmation)
+  #
+  def set_password(password)
+    self.password = password
+    self.password_confirmation = password
   end
 
 end
