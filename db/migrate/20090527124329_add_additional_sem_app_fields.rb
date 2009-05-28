5c41a4917f76375763d34eaab82a26de4352facb
René Sprotte <renspr@uni-paderborn.de>
2009-05-28 14:17:12 +0200

    removed

--- db/migrate/20090527124329_add_additional_sem_app_fields.rb
+++
@@ -1,9 +0,0 @@
-class AddAdditionalSemAppFields < ActiveRecord::Migration
-  def self.up
-    add_column :sem_apps, :active, :boolean, :null => false, :default => false
-  end
-
-  def self.down
-    remove_column :sem_apps, :active
-  end
-end
