d212798948ecc3244f82fdf388f140a243ab0a74
René Sprotte <renspr@uni-paderborn.de>
2009-06-30 14:59:35 +0200

    ...

--- app/controllers/sem_apps_controller.rb
+++ app/controllers/sem_apps_controller.rb
@@ -6,8 +6,9 @@ class SemAppsController < ApplicationController
     # filter conditions
     conditions = ["", {}]
 
-    # only active sem apps
-    add_condition(conditions, "active = :active", {:active => true})
+    # only approved and active sem apps
+    add_condition(conditions, "AND", "approved = :approved", {:approved => true})
+    add_condition(conditions, "AND", "active = :active", {:active => true})
 
     # try to filter by semester
 #    if params[:semester]
@@ -57,11 +58,11 @@ class SemAppsController < ApplicationController
   private
 
   
-  def add_condition(conditions, condition_str, values)
+  def add_condition(conditions, operand, condition_str, values)
     if conditions[0].empty?
       conditions[0] = conditions[0].concat(condition_str)
     else
-      conditions[0] = conditions[0].concat(" AND " + condition_str)
+      conditions[0] = conditions[0].concat(" #{operand || 'AND'} " + condition_str)
     end
     conditions[1] = conditions[1].merge(values)
   end
