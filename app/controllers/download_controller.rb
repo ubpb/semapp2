1611c027e58b8236ab5394e7c7b88348aa6aad1a
René Sprotte <renspr@uni-paderborn.de>
2009-09-02 17:15:42 +0200

    added tiny mce support for text entries

--- app/controllers/download_controller.rb
+++ app/controllers/download_controller.rb
@@ -2,15 +2,15 @@ class DownloadController < ApplicationController
 
   def download
     # load the attachment by the given id
-    file_attachment = SemAppFileAttachment.find(params[:id])
+    file_entry = SemAppFileEntry.find(params[:id])
 
     # TODO: security check
-    sem_app = file_attachment.attachable.sem_app_entry.sem_app
+    sem_app = file_entry.sem_app_entry.sem_app
     # ... check if the user is allowed to read files from this sem_app
 
     # check all relevant elements of the path to defend url guessing
     request_path = "#{params[:hash1]}/#{params[:hash2]}/#{params[:hash3]}/#{params[:id]}/#{params[:style]}"
-    path = file_attachment.attachment.path(params[:style])
+    path = file_entry.attachment.path(params[:style])
     unless path.include?(request_path)
       render :file => "#{RAILS_ROOT}/public/404.html", :layout => false, :status => 404
       return false
@@ -18,9 +18,10 @@ class DownloadController < ApplicationController
 
     # finally send the file
     send_file(path,
-      :filename => file_attachment.attachment_file_name,
+      :stream      => true,
+      :filename    => file_entry.attachment_file_name,
       :disposition => 'attachment',
-      :type => file_attachment.attachment_content_type)
+      :type        => file_entry.attachment_content_type || 'application/octed-stream')
   end
 
 end
