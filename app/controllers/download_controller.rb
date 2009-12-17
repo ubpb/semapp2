448dc065127ba670d821671fa8b91c3c9dae6fd4
René Sprotte <renspr@uni-paderborn.de>
2009-12-17 17:07:22 +0100

    added support for locking/unlocking the media tab and respect this when downloading attachements

--- app/controllers/download_controller.rb
+++ app/controllers/download_controller.rb
@@ -1,24 +1,20 @@
 class DownloadController < ApplicationController
 
   def download
-    # load the attachment by the given id
     attachment = Attachment.find(params[:id])
 
-    # check access
     sem_app = attachment.sem_app_entry.sem_app
-    unless sem_app.is_unlocked_in_session?(session)
+    if sem_app.is_unlocked_in_session?(session) or sem_app.is_editable_for?(current_user)
+      send_file(attachment.attachable.path(params[:style]),
+        :stream      => true,
+        :filename    => attachment.attachable_file_name,
+        :disposition => 'attachment',
+        :type        => attachment.attachable_content_type || 'application/octed-stream'
+      )
+    else
       flash[:error] = 'Zugriff verweigert'
       redirect_to sem_app_path(sem_app, :anchor => 'media')
-      return false
     end
-
-    # finally send the file
-    send_file(attachment.attachable.path(params[:style]),
-      :stream      => true,
-      :filename    => attachment.attachable_file_name,
-      :disposition => 'attachment',
-      :type        => attachment.attachable_content_type || 'application/octed-stream'
-    )
   end
 
 end
