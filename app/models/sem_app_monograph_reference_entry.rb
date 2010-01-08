29c3248517ada82808e5d7f8e4988fe0f3a7dfd2
René Sprotte <renspr@uni-paderborn.de>
2010-01-08 13:28:53 +0100

    added support for attachments on bib references for monographs

--- app/models/sem_app_monograph_reference_entry.rb
+++ app/models/sem_app_monograph_reference_entry.rb
@@ -2,6 +2,9 @@ class SemAppMonographReferenceEntry < SemAppEntry
   set_table_name :sem_app_monograph_reference_entries
 
   belongs_to :sem_app
+  has_many   :attachments, :class_name => '::Attachment', :dependent => :destroy, :foreign_key => 'sem_app_entry_id'
+  accepts_nested_attributes_for :attachments, :allow_destroy => true, :reject_if => proc { |attrs| attrs.all? { |k, v| v.blank? } }
+
   validates_presence_of :author
   validates_presence_of :title
 
