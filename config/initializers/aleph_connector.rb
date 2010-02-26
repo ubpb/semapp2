81bf08082a64063ee38567411146886b1abed0c9
René Sprotte <renspr@uni-paderborn.de>
2010-02-26 11:35:29 +0100

    Accept only PA-Logins

--- config/initializers/aleph_connector.rb
+++ config/initializers/aleph_connector.rb
@@ -1,5 +1,5 @@
 Aleph::Connector.base_url           = 'http://ubaleph.upb.de/X'
 Aleph::Connector.library            = 'pad50'
 Aleph::Connector.search_base        = 'pbaus'
-Aleph::Connector.allowed_user_types = [/^PA.+/, /^PS.+/]
+Aleph::Connector.allowed_user_types = [/^PA.+/]
 Aleph::Connector.allowed_ban_codes  = [/^00$/]
