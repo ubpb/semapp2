6e9c799fc3a2a04e96aca626c833a5ca0f5d114d
René Sprotte <renspr@uni-paderborn.de>
2010-03-05 10:15:12 +0100

    ...

--- app/controllers/download_controller.rb
+++ app/controllers/download_controller.rb
@@ -1,16 +1,22 @@
 class DownloadController < ApplicationController
 
   def download
-    attachment = FileAttachment.find(params[:id])
+    attachment = FileAttachment.find(params[:id], :include => {:entry => [:sem_app]})
 
     sem_app = attachment.entry.sem_app
     if sem_app.is_unlocked_in_session?(session) or can?(:edit, sem_app)
-      send_file(attachment.file.path(params[:style]),
-        :stream      => true,
-        :filename    => attachment.file_file_name,
-        :disposition => 'attachment',
-        :type        => attachment.file_content_type || 'application/octed-stream'
-      )
+      file = attachment.file.path(params[:style])
+      unless File.exists?(file)
+        flash[:error] = 'Diese Datei existiert nicht'
+        redirect_to sem_app_path(sem_app, :anchor => 'media')
+      else
+        send_file(file,
+          :stream      => true,
+          :filename    => attachment.file_file_name,
+          :disposition => 'attachment',
+          :type        => attachment.file_content_type || 'application/octed-stream'
+        )
+      end
     else
       flash[:error] = 'Zugriff verweigert'
       redirect_to sem_app_path(sem_app, :anchor => 'media')
