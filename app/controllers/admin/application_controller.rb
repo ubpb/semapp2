ae9981e0fbd2465a67e940f6391d8c96b6df06c3
René Sprotte <renspr@uni-paderborn.de>
2010-03-17 17:12:41 +0100

    Argh

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -10,7 +10,7 @@ class Admin::ApplicationController < ApplicationController
 
   def secure_controller
     authenticate_user!
-    unauthorized! unless current_user or current_user.is_admin?
+    unauthorized! unless current_user and current_user.is_admin?
   end
 
 end
