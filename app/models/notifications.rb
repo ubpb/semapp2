579d281550e48818444920b51287a7a7c14bdbc3
René Sprotte <renspr@uni-paderborn.de>
2010-03-24 13:28:27 +0100

    improved notification

--- app/models/notifications.rb
+++ app/models/notifications.rb
@@ -3,7 +3,7 @@
 class Notifications < ActionMailer::Base
 
   def sem_app_activated_notification(user, sem_app)
-    if user.present? and user.email.present?
+    if user.present? and user.email.present? and sem_app.present?
       recipients user.email
       from       "information@ub.uni-paderborn.de"
       subject    "[Seminarapparate] Ihr Seminarapparat (#{sem_app.title}) wurde aktiviert"
