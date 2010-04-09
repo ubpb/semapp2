6e801d47bc54ceb8bb427b763f6669e1acbc6257
René Sprotte <renspr@uni-paderborn.de>
2010-04-09 13:09:07 +0200

    Added support for filtering for ils_account and slot_number in admin

--- app/models/sem_apps_filter.rb
+++ app/models/sem_apps_filter.rb
@@ -2,7 +2,7 @@
 
 class SemAppsFilter
 
-  attr_accessor :title, :tutors, :creator, :location, :semester, :unapproved_only, :bookjobs_only
+  attr_accessor :title, :tutors, :creator, :location, :semester, :ils_account, :slot_number, :unapproved_only, :bookjobs_only
 
   def initialize(filter = {})
     filter = {} unless filter
@@ -12,24 +12,28 @@ class SemAppsFilter
     @creator         = filter[:creator]       if filter[:creator].present?
     @location        = filter[:location].to_i if filter[:location].present?
     @semester        = filter[:semester].to_i if filter[:semester].present?
+    @ils_account     = filter[:ils_account]   if filter[:ils_account].present?
+    @slot_number     = filter[:slot_number]   if filter[:slot_number].present?
     @unapproved_only = filter[:only] == "unapproved"
     @bookjobs_only   = filter[:only] == "bookjobs"
   end
 
   def scope
-    scope = SemApp.scoped({:include => [:books]})
+    scope = SemApp.scoped({:include => [:books, :book_shelf]})
     scope = scope.conditions "lower(sem_apps.title) LIKE ?", "%#{@title.downcase}%" unless @title.blank?
     scope = scope.conditions "lower(sem_apps.tutors) LIKE ?", "%#{@tutors.downcase}%" unless @tutors.blank?
-    scope = scope.conditions "lower(users.name) LIKE ? OR lower(users.login) LIKE ?", "%#{@creator.downcase}%", "%#{@creator.downcase}%" unless @creator.blank?
+    scope = scope.conditions "lower(users.name) LIKE ? OR lower(users.login) LIKE ?", "#{@creator.downcase}%", "#{@creator.downcase}%" unless @creator.blank?
     scope = scope.conditions "location_id = ?", "#{@location}" unless @location.blank?
     scope = scope.conditions "semester_id = ?", "#{@semester}" unless @semester.blank?
+    scope = scope.conditions "lower(book_shelves.ils_account) like ?", "#{@ils_account.downcase}%" unless @ils_account.blank?
+    scope = scope.conditions "lower(book_shelves.slot_number) = ?", "#{@slot_number.downcase}" unless @slot_number.blank?
     scope = scope.conditions "approved = ?", false if @unapproved_only
     scope = scope.conditions "books.state = ? OR books.state = ?", Book::States[:ordered], Book::States[:rejected] if @bookjobs_only
     scope
   end
 
   def filtered?
-    [@title, @tutors, @creator, @location, @semester, @unapproved_only, @bookjobs_only].any? {|f| f.present?}
+    [@title, @tutors, @creator, @location, @semester, @ils_account, @slot_number, @unapproved_only, @bookjobs_only].any? {|f| f.present?}
   end
 
 end
