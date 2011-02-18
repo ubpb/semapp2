1bf53042d77afd5d675ac8d92f0d887ca690f63f
René Sprotte <rene.sprotte@provideal.net>
2011-02-18 11:18:45 +0100

    Version bump for Rails.

--- config/environment.rb
+++ config/environment.rb
@@ -3,7 +3,7 @@
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.11' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -15,7 +15,7 @@ Rails::Initializer.run do |config|
 
   # Add additional load paths for your own custom dirs
   # config.load_paths += %W( #{RAILS_ROOT}/extras )
-  
+
   # Specify gems that this application depends on and have them installed with rake gems:install
   config.gem 'RedCloth',      :version => '4.2.3',   :lib => 'RedCloth'
   config.gem 'devise',        :version => '1.0.4',   :lib => 'devise'
