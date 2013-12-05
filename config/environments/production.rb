ed4f4c06bbecb3774687f536e24de38642348f3f
Michael Sievers <michael_sievers@web.de>
2013-12-05 14:11:44 +0100

    Fixed broken css print assets in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ SemApp2::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif views/**/manifest*.js)
+  config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif print.css views/**/manifest*.js)
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
