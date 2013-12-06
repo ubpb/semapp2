6404b5f05c5dc303f5df6174baf8b239eb22b0aa
Michael Sievers <michael_sievers@web.de>
2013-12-06 16:37:45 +0100

    Cleaned up User model further

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,39 +8,32 @@ class User < ActiveRecord::Base
   validates_presence_of :login
   validates_presence_of :name
 
-  class << self
-    def authenticate(attributes)
-      aleph_user = Aleph::Connector.new.authenticate(attributes[:login], attributes[:password])
-      raise "Aleph authentication failed" unless aleph_user && aleph_user.is_a?(Aleph::User)
+  def self.authenticate(attributes)
+    if (aleph_user = Aleph::Connector.new.authenticate(attributes[:login], attributes[:password])).is_a?(Aleph::User)
       create_or_update_aleph_user!(aleph_user)
+    else
+      raise "Aleph authentication failed"
     end
-
-    private
-      def create_or_update_aleph_user!(aleph_user)
-        user = self.find_by_login(aleph_user.user_id)
-        user.present? ? update_user!(user, aleph_user) : create_user!(aleph_user)
-      end
-
-      def create_user!(aleph_user)
-        User.create!(:login => aleph_user.user_id, :name => aleph_user.name, :email => aleph_user.email)
-      end
-
-      def update_user!(user, aleph_user)
-        user.update_attributes!(:name => aleph_user.name, :email => aleph_user.email)
-        user
-      end
   end
 
   def is_admin?
     is_admin
   end
   
+  def to_s
+    ["#{name} (#{login})", "#{email}"].map(&:presence).compact.join(', ').strip
   end
 
-  def to_s
-    s  = "#{self.name} (#{self.login})"
-    s << ", #{self.email}" if self.email.present?
-    return s
+ private
+
+  def self.create_or_update_aleph_user!(aleph_user)
+    if (user = User.find_by(login: aleph_user.user_id)).present?
+      user.update_attributes!(:name => aleph_user.name, :email => aleph_user.email)
+    else
+      User.create!(:login => aleph_user.user_id, :name => aleph_user.name, :email => aleph_user.email)
+    end
+
+    user
   end
 
 end
