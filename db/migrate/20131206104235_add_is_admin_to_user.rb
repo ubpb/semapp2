8d3fbbfdec6665cfd140683dd70970d299bb30c8
Michael Sievers <michael_sievers@web.de>
2013-12-06 15:20:10 +0100

    Removed migration down functionality

--- db/migrate/20131206104235_add_is_admin_to_user.rb
+++ db/migrate/20131206104235_add_is_admin_to_user.rb
@@ -7,6 +7,11 @@ end
 class AddIsAdminToUser < ActiveRecord::Migration
   def admin_authority_name; 'ROLE_ADMIN'; end
 
+  # migration local model to compensate missing model features
+  class User < ActiveRecord::Base
+    has_and_belongs_to_many :authorities
+  end
+
   def up
     add_column :users, :is_admin, :boolean, default: false, null: false; User.reset_column_information
 
@@ -18,16 +23,6 @@ class AddIsAdminToUser < ActiveRecord::Migration
   end
 
   def down
-    if defined?(Authority)
-      User.all.each do |user|
-        if user.is_admin
-          unless user.authorities.any? { |authority| authority.name == admin_authority_name }
-            user.authorities << Authority.find_by(name: admin_authority_name)
-          end
-        end
-      end
-    end
-
     remove_column :users, :is_admin
   end
 end
--- db/migrate/20131206110838_remove_authorities.rb
+++ db/migrate/20131206110838_remove_authorities.rb
@@ -1,31 +1,9 @@
-begin
-  require 'authority'
-rescue LoadError
-end
-
 class RemoveAuthorities < ActiveRecord::Migration
   def up
-    drop_table :authorities_users
-    drop_table :authorities
+    drop_table :authorities_users if table_exists? :authorities_users
+    drop_table :authorities       if table_exists? :authorities
   end
 
   def down
-    if defined?(Authority)
-      create_table :authorities do |t|
-        t.string :name
-        t.timestamps
-      end
-
-      Authority.create!(name: Authority::ADMIN_ROLE)
-
-      create_table :authorities_users do |t|
-        t.references :user
-        t.references :authority
-        t.timestamps
-      end
-
-      add_index :authorities_users, :user_id
-      add_index :authorities_users, :authority_id
-    end
   end
 end
