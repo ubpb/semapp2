472d2b52262ddb4180f6ead6da71f952dff4fb75
René Sprotte <rene.sprotte@provideal.net>
2014-01-29 13:22:04 +0100

    Allow overriding the Aleph base url by environment valriables.

--- config/initializers/aleph_connector.rb
+++ config/initializers/aleph_connector.rb
@@ -1,6 +1,6 @@
 require 'aleph_connector'
 
-Aleph::Connector.base_url           = 'https://ubex/X'
+Aleph::Connector.base_url           = ENV['ALEPH_BASE_URL'].presence || 'https://ubex/X'
 Aleph::Connector.library            = 'pad50'
 Aleph::Connector.search_base        = 'pbaus'
 Aleph::Connector.allowed_user_types = [/^PA.+/, /\APS.+\Z/]
