1926686d2ddfbad37795c471526681ff1103e5d0
René Sprotte <rene.sprotte@provideal.net>
2014-01-29 13:17:56 +0100

    Connect to ubex by default.

--- config/initializers/aleph_connector.rb
+++ config/initializers/aleph_connector.rb
@@ -1,6 +1,6 @@
 require 'aleph_connector'
 
-Aleph::Connector.base_url           = 'https://ubaleph.upb.de/X'
+Aleph::Connector.base_url           = 'https://ubex/X'
 Aleph::Connector.library            = 'pad50'
 Aleph::Connector.search_base        = 'pbaus'
 Aleph::Connector.allowed_user_types = [/^PA.+/, /\APS.+\Z/]
