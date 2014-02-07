dd0fc45d63a5b359ad8b6d7ca0f09071c621e62d
René Sprotte <rene.sprotte@provideal.net>
2014-02-07 15:18:37 +0100

    Include a new clone service.

---
+++ app/services/sem_app_cloner.rb
@@ -0,0 +1,54 @@
+class SemAppCloner
+
+  def initialize(source_sem_app, target_sem_app, clone_books: true, clone_media: true)
+    @source_sem_app = source_sem_app
+    @target_sem_app = target_sem_app
+    @clone_books    = clone_books
+    @clone_media    = clone_media
+  end
+
+  def clone!
+    SemApp.transaction do
+      clone_books! if @clone_books
+      clone_media! if @clone_media
+    end
+  end
+
+private
+
+  def clone_books!
+    @source_sem_app.books.each do |book|
+      clone = book.dup
+      clone.sem_app = @target_sem_app
+      clone.save!(validate: false)
+    end
+  end
+
+  def clone_media!
+    @source_sem_app.media.each do |media|
+      clone = media.dup(include: :instance, validate: false)
+
+      clone.sem_app          = @target_sem_app
+      clone.file_attachments = media_file_attachments(media)
+
+      clone.save!(validate: false)
+    end
+  end
+
+  def media_file_attachments(media)
+    media.file_attachments.map do |a|
+      path = a.file.path
+
+      if File.exists?(path)
+        attachment = FileAttachment.new(
+          file: File.new(path),
+          description: a.description,
+          scanjob: a.scanjob
+        )
+        attachment.file.instance_write(:file_name, a.file_file_name)
+        attachment
+      end
+    end.compact
+  end
+
+end
--- app/services/sem_app_transit.rb
+++ app/services/sem_app_transit.rb
@@ -12,8 +12,9 @@ class SemAppTransit
       target_sem_app = @source_sem_app.dup(include: :book_shelf, validate: false)
 
       import_basics!(target_sem_app)
-      import_books!(target_sem_app) if @import_books
-      import_media!(target_sem_app) if @import_media
+
+      cloner = SemAppCloner.new(@source_sem_app, target_sem_app, clone_books: @import_books, clone_media: @import_media)
+      cloner.clone!
 
       return target_sem_app
     end
@@ -32,45 +33,6 @@ private
 
     target_sem_app.save!(validate: false)
     target_sem_app
-  end
-
-  def import_books!(target_sem_app)
-    @source_sem_app.books.each do |book|
-      clone = book.dup
-      clone.sem_app = target_sem_app
-      clone.save!(validate: false)
-    end
-
-    target_sem_app
-  end
-
-  def import_media!(target_sem_app)
-    @source_sem_app.media.each do |media|
-      clone = media.dup(include: :instance, validate: false)
-
-      clone.sem_app          = target_sem_app
-      clone.file_attachments = media_file_attachments(media)
-
-      clone.save!(validate: false)
-    end
-
-    target_sem_app
-  end
-
-  def media_file_attachments(media)
-    media.file_attachments.map do |a|
-      path = a.file.path
-
-      if File.exists?(path)
-        attachment = FileAttachment.new(
-          file: File.new(path),
-          description: a.description,
-          scanjob: a.scanjob
-        )
-        attachment.file.instance_write(:file_name, a.file_file_name)
-        attachment
-      end
-    end.compact
   end
 
 end
