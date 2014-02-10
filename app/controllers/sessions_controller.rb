73554d9c5d8095f32ffe1687bfeea89564bffffe
René Sprotte <rene.sprotte@provideal.net>
2014-02-10 12:06:01 +0100

    Don't check students.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -22,25 +22,20 @@ class SessionsController < ApplicationController
   protected
 
     def authenticate(attributes)
-      if attributes["login"].upcase.starts_with?('PS') && User.find_by(login: attributes["login"]).nil?
-        flash.now.alert = 'Anmeldung fehlgeschlagen. Sie müssen von einem Dozenten authorisiert werden.'
-        nil
-      else
-        begin
-          return User.authenticate(attributes)
-        rescue Aleph::AuthenticationError
-          flash.now.alert = 'Anmeldung fehlgeschlagen. Überprüfen Sie Login und Passwort.'
-        rescue Aleph::UnsupportedAccountTypeError
-          flash.now.alert = 'Anmeldung nicht möglich. Dei Anmeldung setzt einen A-Ausweis voraus.'
-        rescue Aleph::AccountLockedError
-          flash.now.alert = 'Ihr Konto wurde gesperrt. Bitte wenden Sie sich an die Bibliothek.'
-        rescue Exception => e
-          puts e.message
-          puts e.backtrace
-          flash.now[:error] = 'Anmeldung fehlgeschlagen. Überprüfen Sie Login und Passwort.'
-        end
-        nil
+      begin
+        return User.authenticate(attributes)
+      rescue Aleph::AuthenticationError
+        flash.now.alert = 'Anmeldung fehlgeschlagen. Überprüfen Sie Login und Passwort.'
+      rescue Aleph::UnsupportedAccountTypeError
+        flash.now.alert = 'Anmeldung nicht möglich. Dei Anmeldung setzt einen A-Ausweis voraus.'
+      rescue Aleph::AccountLockedError
+        flash.now.alert = 'Ihr Konto wurde gesperrt. Bitte wenden Sie sich an die Bibliothek.'
+      rescue Exception => e
+        puts e.message
+        puts e.backtrace
+        flash.now[:error] = 'Anmeldung fehlgeschlagen. Überprüfen Sie Login und Passwort.'
       end
+      nil
     end
 
 end
