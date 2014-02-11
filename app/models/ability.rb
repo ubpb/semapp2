594a6e6cb3045a51ae2b0dcca09b61f827c660de
René Sprotte <rene.sprotte@provideal.net>
2014-02-11 11:37:07 +0100

    Fixed authorization stuff.

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,26 +2,31 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    if user.present? and user.is_admin?
+    if user.is_admin?
       can :manage, :all
     else
       #
       # Sem Apps
       #
       can :read, SemApp do |sem_app|
-        sem_app.approved? || (user.present? && sem_app.owned_by?(user))
+        sem_app.approved? || sem_app.owned_by?(user)
       end
 
       can :create, SemApp do |sem_app|
-        user.present? && user.is_lecturer?
+        user.is_lecturer?
       end
 
       can :edit, SemApp do |sem_app|
-        user.present? && sem_app.owned_by?(user) && !sem_app.archived
+        sem_app.owned_by?(user) && !sem_app.archived
+      end
+
+      can :manage, SemApp do |sem_app|
+        sem_app.creator == user
       end
 
       can :transit, SemApp do |sem_app|
-        sem_app.semester.id == ApplicationSettings.instance.transit_source_semester.id
+        ApplicationSettings.instance.transit_target_semester.present? &&
+          (sem_app.semester.id == ApplicationSettings.instance.transit_source_semester.id)
       end
     end
   end
