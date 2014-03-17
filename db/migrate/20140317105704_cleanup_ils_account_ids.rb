717ebbdbbb7a9178c46e572d52dd576276758651
René Sprotte <rene.sprotte@provideal.net>
2014-03-17 15:01:39 +0100

    !!!!!!!!!

--- db/migrate/20140317105704_cleanup_ils_account_ids.rb
+++ db/migrate/20140317105704_cleanup_ils_account_ids.rb
@@ -2,9 +2,6 @@ class CleanupIlsAccountIds < ActiveRecord::Migration
   def up
     BookShelf.all.each do |bs|
       if bs.ils_account.present?
-        t = BookShelf.clean_ils_account(bs.ils_account)
-        binding.pry if t.nil?
-
         bs.ils_account = bs.ils_account
         bs.save(validate: false)
       end
