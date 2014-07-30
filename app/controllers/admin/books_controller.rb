4633de42cf385f3c319716f94ba3d8f6a72bf787
René Sprotte <rene.sprotte@provideal.net>
2014-07-30 09:28:25 +0200

    Code cleanup

--- app/controllers/admin/books_controller.rb
+++ app/controllers/admin/books_controller.rb
@@ -1,22 +1,25 @@
 class Admin::BooksController < Admin::ApplicationController
 
+  before_filter :load_book
+
+
   def edit
-    @book = Book.find(params[:id])
     @placeholders = placeholders
   end
 
   def update
-    @book = Book.find(params[:id])
     @placeholders = placeholders
 
     placeholder_id = BookShelf.clean_ils_account(params[:book][:placeholder])
-    if placeholder_id.present? && placeholder = @placeholders.where('book_shelves.ils_account = ?', placeholder_id).first
+    placeholder    = @placeholders.where('book_shelves.ils_account = ?', placeholder_id).first
+
+    if placeholder_id.present? && placeholder.present?
       @book.placeholder_id = placeholder.id
       @book.state = :in_shelf
 
       @book.save(validate: false)
       flash[:success] = "Erfolgreich gespeichert"
-      redirect_to admin_sem_app_path(@book.sem_app, :anchor => 'new-books')
+      redirect_to_new_books
     else
       flash.now[:error] = "Konnte nicht gespeichert werden. Überprüfen Sie Ihre Eingabe"
       render :edit
@@ -24,65 +27,73 @@ class Admin::BooksController < Admin::ApplicationController
   end
 
   def defer
-    @book = Book.find(params[:id])
     unless @book.set_state(:deferred)
-      flash[:error] = "Es ist ein Fehler aufgetreten."
+      set_common_flash_error
     end
 
-    redirect_to admin_sem_app_path(@book.sem_app, :anchor => 'new-books')
+    redirect_to_new_books
   end
 
   def dedefer
-    @book = Book.find(params[:id])
     unless @book.set_state(:ordered)
-      flash[:error] = "Es ist ein Fehler aufgetreten."
+      set_common_flash_error
     end
 
-    redirect_to admin_sem_app_path(@book.sem_app, :anchor => 'new-books')
+    redirect_to_new_books
   end
 
   def placed_in_shelf
-    @book = Book.find(params[:id])
     unless @book.set_state(:in_shelf)
-      flash[:error] = "Es ist ein Fehler aufgetreten."
+      set_common_flash_error
     end
 
-    redirect_to admin_sem_app_path(@book.sem_app, :anchor => 'new-books')
+    redirect_to_new_books
   end
 
   def removed_from_shelf
-    @book = Book.find(params[:id])
     unless @book.destroy
-      flash[:error] = "Es ist ein Fehler aufgetreten."
+      set_common_flash_error
     end
 
-    redirect_to admin_sem_app_path(@book.sem_app, :anchor => 'removed-books')
+    redirect_to_removed_books
   end
 
   def reference
-    @book = Book.find(params[:id])
-    ref_type = params[:ref_type]
-
-    unless @book.set_state(:in_shelf) and @book.update_attribute(:reference_copy, ref_type)
-      flash[:error] = "Es ist ein Fehler aufgetreten."
+    unless @book.set_state(:in_shelf) and @book.update_attribute(:reference_copy, params[:ref_type])
+      set_common_flash_error
     end
 
-    redirect_to admin_sem_app_path(@book.sem_app, :anchor => 'new-books')
+    redirect_to_new_books
   end
 
   def destroy
-    @book = Book.find(params[:id])
     unless @book.destroy
       flash[:error] = "Es ist ein Fehler aufgetreten."
     end
 
-    redirect_to admin_sem_app_path(@book.sem_app, :anchor => 'new-books')
+    redirect_to_new_books
   end
 
 private
 
+  def load_book
+    @book = Book.find(params[:id])
+  end
+
   def placeholders
     SemApp.includes(:book_shelf).joins(:book_shelf).where(semester_id: Semester.current.id).order('ils_account')
+  end
+
+  def set_common_flash_error
+    flash[:error] = "Es ist ein Fehler aufgetreten."
+  end
+
+  def redirect_to_new_books
+    redirect_to admin_sem_app_path(@book.sem_app, :anchor => 'new-books')
+  end
+
+  def redirect_to_removed_books
+    redirect_to admin_sem_app_path(@book.sem_app, :anchor => 'removed-books')
   end
 
 end
