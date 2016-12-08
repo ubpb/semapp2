2b0d7dbb5bd9478eb908153ebc0877eca88671d0
René Sprotte <rene.sprotte@provideal.net>
2016-12-08 10:40:20 +0100

    Now the working fix :smile:

--- app/controllers/admin/book_shelves_controller.rb
+++ app/controllers/admin/book_shelves_controller.rb
@@ -4,16 +4,16 @@ class Admin::BookShelvesController < Admin::ApplicationController
     if params[:filter].present? and params[:filter][:location].present?
       location_id = params[:filter][:location]
 
-      if location.present?
+      if location_id.present?
         @shelves = BookShelf
-          .includes(:sem_app => :location)
+          .joins(:sem_app => :location)
           .where("sem_apps.location_id = :location_id AND sem_apps.semester_id = :semester_id",
             location_id: location_id,
             semester_id: Semester.current.id
           )
           .to_a
 
-        @shelves.sort! {|x, y| x.slot_number.to_i <=> y.slot_number.to_i }
+        @shelves.sort!{|x, y| x.slot_number.to_i <=> y.slot_number.to_i }
       end
     end
   end
