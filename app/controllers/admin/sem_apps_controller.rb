595517f3617d3385baab9fad3895aee47c023267
René Sprotte <renspr@gmail.com>
2017-10-18 15:02:45 +0200

    Fix permitted params.

--- app/controllers/admin/sem_apps_controller.rb
+++ app/controllers/admin/sem_apps_controller.rb
@@ -18,8 +18,12 @@ class Admin::SemAppsController < Admin::ApplicationController
   end
 
   def show
-    @sem_app        = SemApp.find_by_id(params[:id])
-    (flash[:error] = "Dieser Apparat existiert nicht"; redirect_to admin_sem_apps_path) unless @sem_app.present?
+    @sem_app = SemApp.find_by_id(params[:id])
+
+    unless @sem_app.present?
+      flash[:error] = "Dieser Apparat existiert nicht"
+      redirect_to admin_sem_apps_path and return
+    end
 
     @ordered_books  = Book.for_sem_app(@sem_app).ordered.ordered_by('signature asc')
     @removed_books  = Book.for_sem_app(@sem_app).removed.ordered_by('signature asc')
@@ -42,7 +46,7 @@ class Admin::SemAppsController < Admin::ApplicationController
 
     was_approved = @sem_app.approved
 
-    if @sem_app.update_attributes(params[:sem_app])
+    if @sem_app.update_attributes(permitted_params)
       if (@sem_app.approved and not was_approved and @sem_app.creator.present? and @sem_app.creator.email.present?)
         Notifications.sem_app_activated_notification(@sem_app).deliver
       end
@@ -89,6 +93,22 @@ class Admin::SemAppsController < Admin::ApplicationController
     @sem_app.destroy
     flash[:success] = "Seminarpparat '#{@sem_app.title}' gelöscht."
     redirect_to :action => :index
+  end
+
+private
+
+  def permitted_params
+    params.require(:sem_app).permit(
+      :approved,
+      :semester_id,
+      :title,
+      :course_id,
+      :tutors,
+      :location_id,
+      :shared_secret,
+      book_shelf_attributes: [:slot_number, :ils_account, :_destroy, :id],
+      book_shelf_ref_attributes: [:sem_app_ref_id],
+    )
   end
 
 end
