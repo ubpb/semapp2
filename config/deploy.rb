1f855dc778ef9c838be5e41622de6d192e780d48
René Sprotte <renspr@gmail.com>
2018-04-12 14:21:03 +0200

    Add task to pull the remote db for local testing.

--- config/deploy.rb
+++ config/deploy.rb
@@ -18,7 +18,6 @@ set :rvm_ruby_version, "default"
 set :rails_env, "production"
 
 namespace :deploy do
-
   after :publishing, :restart_app do
     on roles(:web) do
       within release_path do
@@ -26,5 +25,43 @@ namespace :deploy do
       end
     end
   end
+end
 
+namespace :app do
+  namespace :db do
+    desc 'Pull db from remote server'
+    task :pull do
+      # Find the first server in role 'db' (all db servers read the same database)
+      server = Capistrano::Configuration.env.send(:servers).find{ |s| s.roles.include?(:db) }
+      raise "No proper server found" if server.nil?
+
+      # Setup variables
+      dump_file_name = "semapp-#{Time.now.strftime("%Y%m%d-%H%M%S")}.dump"
+      remote_db = "semapp"
+      local_db = "semapp2"
+      remote_dump_file = "/tmp/#{dump_file_name}"
+      local_dump_file = "/tmp/#{dump_file_name}"
+
+      # Dump db on remote server
+      on(server) do |host|
+        execute("export PGPASSWORD=#{ENV['REMOTE_DB_PASSWORD']} ; pg_dump -x -O -F c -f #{remote_dump_file} -U postgres #{remote_db}")
+      end
+
+      # Download file
+      on(server) do |host|
+        download!(remote_dump_file, local_dump_file)
+      end
+
+      # Restore dump locally
+      system("pg_restore -x -O -c -d #{local_db} #{local_dump_file}")
+
+      # Delete dump on remote server
+      on(server) do |host|
+        execute("rm #{remote_dump_file}")
+      end
+
+      # Delete dump locally
+      system("rm #{local_dump_file}")
+    end
+  end
 end
