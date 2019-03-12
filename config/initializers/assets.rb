916521cfd858953170a8054812278e937a4c23ea
René Sprotte <renspr@gmail.com>
2019-03-12 16:46:13 +0100

    Add print.css to precompile list.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -18,3 +18,5 @@ Rails.application.config.assets.precompile += %w( views/**/*.js )
 
 Rails.application.config.assets.precompile += %w( markitup/sets/textile/**/*.css markitup/sets/textile/**/*.png markitup/sets/textile/**/*.js )
 Rails.application.config.assets.precompile += %w( markitup/skins/pui/**/*.css markitup/skins/pui/**/*.png markitup/skins/pui/**/*.js )
+
+Rails.application.config.assets.precompile += %w( print.css )
