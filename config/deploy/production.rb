4bbdd37ae1db50a2d8ffc7b1e8d6063e5e4b0006
René Sprotte <renspr@gmail.com>
2019-07-16 14:53:14 +0200

    Add ubatch18 to production deployment.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,5 @@
-server "ubpegasus18", user: "ubpb", roles: %w{app web db}
+server "ubpegasus18", user: "ubpb", roles: %w{app web}
 server "ubperseus18", user: "ubpb", roles: %w{app web}
+server "ubatch18",    user: "ubpb", roles: %w{app db}
 set :deploy_to, "/ubpb/semapp"
 set :branch, "production"
