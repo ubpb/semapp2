84da905a941df0dfeb0f8a3aef477c9c21ab521d
René Sprotte <renspr@gmail.com>
2021-10-25 15:00:43 +0200

    Make list unique in case there are more than one Book Shelf per SemApp.

--- app/controllers/admin/book_shelves_controller.rb
+++ app/controllers/admin/book_shelves_controller.rb
@@ -7,10 +7,12 @@ class Admin::BookShelvesController < Admin::ApplicationController
       if location_id.present?
         @shelves = BookShelf
           .joins(:sem_app => :location)
+          .includes(:sem_app)
           .where("sem_apps.location_id = :location_id AND sem_apps.semester_id = :semester_id",
             location_id: location_id,
             semester_id: Semester.current.id
           )
+          .uniq(&:sem_app)
           .to_a
 
         @shelves.sort!{|x, y| x.slot_number.to_i <=> y.slot_number.to_i }
