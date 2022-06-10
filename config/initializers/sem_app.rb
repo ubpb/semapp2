e0f4822c16677f5a6acd451804b694f2856174dd
René Sprotte <renspr@users.noreply.github.com>
2022-06-10 14:28:54 +0200

    Add Alma API config.

--- config/initializers/sem_app.rb
+++ config/initializers/sem_app.rb
@@ -6,6 +6,14 @@ module SemApp2
   def self.config=(hash)
     @@config = OpenStruct.new(hash)
   end
+
+  def self.alma_api
+    @@alma_api ||= ExlApi.configure do |config|
+      config.api_key      = self.config.alma_api_key
+      config.api_base_url = self.config.alma_api_base_url || "https://api-eu.hosted.exlibrisgroup.com/almaws/v1"
+      config.language     = self.config.alma_api_language || "de"
+    end
+  end
 end
 
 SemApp2.config = YAML.load_file("config/sem_app.yml")[Rails.env]
