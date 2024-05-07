a55b0896e8b4d9685515fb52a72e2bd53fd66b71
René Sprotte <renspr@users.noreply.github.com>
2024-05-07 15:50:05 +0200

    Fix missing RestClient.

--- lib/alma_sync_engine_adapter.rb
+++ lib/alma_sync_engine_adapter.rb
@@ -93,8 +93,8 @@ private
     sru_url = "https://hbz-pad.alma.exlibrisgroup.com/view/sru/49HBZ_PAD?version=1.2&operation=searchRetrieve&recordSchema=dcx&query=alma.local_field_981=#{aleph_id}"
 
     mms_id = nil
-    response = RestClient.get(sru_url)
-    if response.code == 200
+    response = Faraday.get(sru_url)
+    if response.status == 200
       doc = Nokogiri::XML(response.body).remove_namespaces!
       mms_id = doc.at_xpath("//records/record/recordIdentifier")&.text
     end
