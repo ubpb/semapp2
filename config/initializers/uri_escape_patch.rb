0a82804fc31faa4f1e45a8aa55dbbf0375a79aec
René Sprotte <renspr@users.noreply.github.com>
2024-05-14 10:05:15 +0200

    Bring back URI#escape/URI#unescape as some Gems we can not update rely on.

--- config/initializers/uri_escape_patch.rb
+++ config/initializers/uri_escape_patch.rb
@@ -1,12 +1,17 @@
-# URI#escape was deprecated and later removed
-# This is a monkey patch to bring it back unsing CGI.escape
+# URI#escape was deprecated and later removed.
+# This is a monkey patch to bring it back as some gems (Paperclip) still use it.
+# Taken from https://gist.github.com/corytheboyd-smartsheet/6d39344a8d03713d8a345d80d97ec076
 
-require "uri"
-
-module URI
+URI.class_eval do
   class << self
-    def escape(str)
-      CGI.escape(str)
+    def escape(*arg)
+      URI::DEFAULT_PARSER.escape(*arg)
     end
+    alias encode escape
+
+    def unescape(*arg)
+      URI::DEFAULT_PARSER.unescape(*arg)
+    end
+    alias decode unescape
   end
 end
