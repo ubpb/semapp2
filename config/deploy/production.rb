f82fee4ceebf6d033bfcad25044fcffd292aa36e
René Sprotte <renspr@users.noreply.github.com>
2024-09-11 15:41:48 +0200

    Switch to batch.ub for production deployments.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,5 @@
 server "ubpegasus18.upb.de", user: "ubpb", roles: %w{app web}
 server "ubperseus18.upb.de", user: "ubpb", roles: %w{app web}
-server "ubatch18.upb.de",    user: "ubpb", roles: %w{app db}
+server "batch.ub.upb.de",    user: "ubpb", roles: %w{app db}
 set :deploy_to, "/ubpb/semapp"
 set :branch, "production"
