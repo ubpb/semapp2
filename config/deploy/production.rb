5f15c1e4a6c1d27ed48fcb9f5333d11e6c641f48
René Sprotte <renspr@users.noreply.github.com>
2024-09-13 11:54:58 +0200

    Add app1 and app2.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,7 @@
 server "ubpegasus18.upb.de", user: "ubpb", roles: %w{app web}
 server "ubperseus18.upb.de", user: "ubpb", roles: %w{app web}
+server "app1.ub.upb.de",     user: "ubpb", roles: %w{app web}
+server "app2.ub.upb.de",     user: "ubpb", roles: %w{app web}
 server "batch.ub.upb.de",    user: "ubpb", roles: %w{app db}
 set :deploy_to, "/ubpb/semapp"
 set :branch, "production"
