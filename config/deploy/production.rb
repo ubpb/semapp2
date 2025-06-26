833cd16339068c39976524b34b3213715aec4ebd
René Sprotte <renspr@users.noreply.github.com>
2025-06-26 11:14:11 +0200

    Remove old/unused servers.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,3 @@
-server "ubpegasus18.upb.de", user: "ubpb", roles: %w{app web}
-server "ubperseus18.upb.de", user: "ubpb", roles: %w{app web}
 server "app1.ub.upb.de",     user: "ubpb", roles: %w{app web}
 server "app2.ub.upb.de",     user: "ubpb", roles: %w{app web}
 server "batch.ub.upb.de",    user: "ubpb", roles: %w{app db}
